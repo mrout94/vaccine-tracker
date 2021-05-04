e8a94e64e684c1793f52fc5a988b19668f9256f3
Manabendra Rout <56976500+mrout94@users.noreply.github.com>
2021-05-04 05:47:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def state_options():
         Names fetched from government server.
     '''
     
-    path = '.././Data/State_Names.csv'
+    path = './Data/State_Names.csv'
     df = pd.read_csv(path)
     state_names = df['state_name'].unique().tolist()
     return state_names
@@ -41,7 +41,7 @@ def district_options(state_name):
         List of all districts in the given state
     '''
     
-    path = '.././Data/State_And_Disctrict_Names.csv'
+    path = './Data/State_And_Disctrict_Names.csv'
     df = pd.read_csv(path)
     df = df[df['state_name']==state_name]
     district_names = df['district_name'].unique().tolist()
@@ -70,13 +70,13 @@ def get_Vax_Data(state, district, age):
         List of all unique pincodes in a given district.
     '''
     
-    path = '.././Data/State_And_Disctrict_Names.csv'
+    path = './Data/State_And_Disctrict_Names.csv'
     df = pd.read_csv(path)
     DIST_ID = df[(df['district_name'] == district) & (df['state_name'] == state)]['district_id'].values[0]
     START_DATE = datetime.today().strftime('%d-%m-%Y')
     VAX_DATA_PORTAL = f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={DIST_ID}&date={START_DATE}'
     res = requests.get(VAX_DATA_PORTAL)
-    vax_info_df = pd.DataFrame(json.loads(res.text)['centers'])
+    vax_info_df = pd.DataFrame(json.loads(res.text)["centers"])
     vax_info_df["from"] = pd.to_datetime(vax_info_df["from"]).dt.strftime('%H:%M')
     vax_info_df["to"] = pd.to_datetime(vax_info_df["to"]).dt.strftime('%H:%M')
     vax_info_df["Timing"] = vax_info_df["from"] + " - " + vax_info_df["to"]
@@ -93,73 +93,77 @@ def get_Vax_Data(state, district, age):
     vax_info_all_sessions_df = vax_info_all_sessions_df[vax_info_all_sessions_df['Min_Age'] == age]
     return vax_info_all_sessions_df, pincode_list
 
-
-st.set_page_config(page_title='Vaccine Slot Tracker' , page_icon=":syringe:",layout='wide', initial_sidebar_state='auto')
-st.title('CoWin : Vaccine Availability :syringe:')
-
-state = st.sidebar.selectbox("Choose Your State", state_options())
-district = st.sidebar.selectbox("Choose Your District", district_options(state))
-age = st.sidebar.slider("Choose Your Age", min_value=0, max_value=100, step=1, value=45)
-if age < 18:
-    age=0
-elif (age>=18) and (age<45):
-    age=18
-elif age > 45:
-    age=45
-
-progress_bar = st.progress(0)
-progress_message = st.empty()
-
-try:
-    progress_message.text('Fetching data. Please wait...')
-    progress_bar.progress(10)
-    vax_df, pincode_list = get_Vax_Data(state, district, age)
-    progress_bar.progress(100)
+if __name__ == '__main__':
+    st.set_page_config(page_title='Vaccine Tracker: India' , page_icon=":syringe:",layout='wide', initial_sidebar_state='auto')
+    st.title(':syringe: Vaccine Tracker: India')
     
-    # Filter Based on PINCODE
-    pincode = None
-    search_by_pin = st.sidebar.checkbox('Filter by Pincode')
-    if search_by_pin:
-        selected_pincode = st.sidebar.selectbox("Select Pincode",options=sorted(set(pincode_list)))
-        vax_df = vax_df[vax_df["pincode"] == selected_pincode]
+    state = st.sidebar.selectbox("Choose Your State", state_options())
+    district = st.sidebar.selectbox("Choose Your District", district_options(state))
+    age = st.sidebar.slider("Choose Your Age", min_value=0, max_value=100, step=1, value=45)
+    if age < 18:
+        age=0
+    elif (age>=18) and (age<45):
+        age=18
+    elif age > 45:
+        age=45
     
-    #Filter Based on Availability
-    only_available = st.sidebar.checkbox('Show only Available')
-    if only_available:
-        vax_df = vax_df[vax_df["Doses_Available"] > 0]
+    progress_bar = st.progress(0)
+    progress_message = st.empty()
     
-    # Only Selecting Important Columns
-    cols_to_show = ['name', 'state_name', 'district_name', 'pincode', 'from', 'to', 'fee_type', 'Min_Age', 'Doses_Available',
-                    'Vaccine', 'Date']
-    vax_df = vax_df[cols_to_show]
-    plot_df = vax_df.groupby(['Date']).sum()['Doses_Available'].reset_index()
-    fig = go.Figure([go.Bar(x=plot_df['Date'], y=plot_df['Doses_Available'])])
+    try:
+        progress_message.text('Fetching data. Please wait...')
+        progress_bar.progress(10)
+        vax_df, pincode_list = get_Vax_Data(state, district, age)
+        progress_bar.progress(100)
+        
+        # Filter Based on PINCODE
+        pincode = None
+        search_by_pin = st.sidebar.checkbox('Filter by Pincode')
+        if search_by_pin:
+            selected_pincode = st.sidebar.multiselect("Select Pincode", default=sorted(set(pincode_list))[0],
+                                                      options=sorted(set(pincode_list)))
+            vax_df = vax_df[vax_df["pincode"].isin(selected_pincode)]
+        
+        #Filter Based on Availability
+        only_available = st.sidebar.checkbox('Show only Available')
+        if only_available:
+            vax_df = vax_df[vax_df["Doses_Available"] > 0]
+        
+        # Only Selecting Important Columns
+        cols_to_show = ['name', 'state_name', 'district_name', 'pincode', 'from', 'to', 'fee_type', 'Min_Age', 'Doses_Available',
+                        'Vaccine', 'Date']
+        vax_df = vax_df[cols_to_show]
+        plot_df = vax_df.groupby(['Date']).sum()['Doses_Available'].reset_index()
+        fig = go.Figure([go.Bar(x=plot_df['Date'], y=plot_df['Doses_Available'])])
+        
+        vax_df.rename(columns={'name':'Name', 'state_name':'State', 'district_name':'District', 'pincode':'Pincode', 'from':'From',
+                               'to': 'To', 'fee_type': 'Free/Paid', 'Min_Age':'Min Age', 'Doses_Available':'Available Doses',
+                               'Vaccine':'Vaccine', 'Date':'Date'}, inplace=True)
+        
+        st.markdown(f'## Availability in {district} ({state})')
+        st.plotly_chart(fig, use_container_width=True)
+        st.markdown('## Raw Data')
+        st.table(vax_df)
+        
+        #Re-Run the search
+        st.sidebar.button("Re-run")
+        
+        # Support Me
+        st.sidebar.markdown('**Collaborate:-**')
+        st.sidebar.markdown('Source: [Github](https://github.com/mrout94/vaccine-tracker) :star:')
+        st.sidebar.markdown('Connect: [LinkedIn](https://www.linkedin.com/in/manabendrarout/) :handshake:')
+        # st.sidebar.markdown('Data Science: [Kaggle](https://www.kaggle.com/manabendrarout) :duck:')
+        st.sidebar.markdown('*Created By:- Manabendra Rout*')
     
-    vax_df.rename(columns={'name':'Name', 'state_name':'State', 'district_name':'District', 'pincode':'Pincode', 'from':'From',
-                           'to': 'To', 'fee_type': 'Free/Paid', 'Min_Age':'Min Age', 'Doses_Available':'Available Doses',
-                           'Vaccine':'Vaccine', 'Date':'Date'}, inplace=True)
+    except URLError as e:
+        st.error(
+            """
+            **This App requires internet access.**
     
-    st.markdown(f'## Availability in {district} ({state})')
-    st.plotly_chart(fig, use_container_width=True)
-    st.markdown('## Raw Data')
-    st.table(vax_df)
-    
-    # Support Me
-    st.sidebar.markdown('**Collborate:-**')
-    st.sidebar.markdown('Source: [Github](https://github.com/mrout94/vaccine-tracker) :star:')
-    st.sidebar.markdown('Connect: [LinkedIn](https://www.linkedin.com/in/manabendrarout/) :handshake:')
-    st.sidebar.markdown('Data Science: [Kaggle](https://www.kaggle.com/manabendrarout) :duck:')
-    st.sidebar.markdown('*Created By:- Manabendra Rout*')
-
-except URLError as e:
-    st.error(
+            Connection error: %s
         """
-        **This App requires internet access.**
-
-        Connection error: %s
-    """
-        % e.reason
-    )
-
-progress_bar.empty()
-progress_message.empty()
+            % e.reason
+        )
+    
+    progress_bar.empty()
+    progress_message.empty()
